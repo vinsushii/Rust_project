640d0a2a969a700c6ce8b32bd31535c57c8f3997
me_yanna <102679263+LacadenJulianna@users.noreply.github.com>
2025-08-31 10:35:28 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write, BufReader};
+use std::path::Path;
+use std::process;
+
+
+fn menu() {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+   
 }
