b82e619c1003fd562eab2535038b704b257afd3f
Julianna Lacaden <jraine.lacaden@gmail.com>
2025-09-01 13:29:22 +0800

    Initial update on main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,26 @@ use std::io::{self, Write, BufReader};
 use std::path::Path;
 use std::process;
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
+enum Status {
+    Pending,
+    InProgress,
+    Resolved,
+}
 
-fn menu() {
-    
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Incident {
+    id: u32,
+    datetime: String,
+    people_involved: Vec<String>,
+    description: String,
+    status: Status,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Database {
+    incidents: HashMap<u32, Incident>,
+    last_id: u32,
 }
 
 fn main() {
