287e73615e4b8e191f4e5e8e0bd85901a728507a
Hiptree <163296939+Hiptree@users.noreply.github.com>
2025-09-02 11:05:52 +0800

    Update database.rs

--- src/database.rs
+++ src/database.rs
@@ -1,1 +1,23 @@
+#[derive(Debug, Serialize, Deserialize)]
+struct Database {
+    incidents: HashMap<u32, Incident>,
+    last_id: u32,
+}
 
+impl Database {
+    fn new() -> Self {
+        Database {
+            incidents: HashMap::new(),
+            last_id: 0,
+        }
+    }
+
+    fn update_status(&mut self, id: u32, new_status: Status) {
+        if let Some(incident) = self.incidents.get_mut(&id) {
+            incident.status = new_status;
+            println!("Incident {} updated to {:?}", id, incident.status);
+        } else {
+            println!("Incident ID {} not found", id);
+        }
+    }
+}
